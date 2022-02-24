d22e5b234a5ba16736b3a083e05e83808269d0a7
ruslanyar <styxrus@yandex.ru>
2022-02-24 15:50:11 +0300

    BurgerIngredients without styles

---
+++ src/components/burger-ingredients-item/BurgerIngredientsItem.jsx
@@ -0,0 +1,19 @@
+import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import itemStyles from './BurgerIngredientsItem.module.css';
+
+const BurgerIngredientsItem = ({ingredient}) => {
+
+	return (
+		<div className={itemStyles.item}>
+			<Counter count={1} size="default" />
+			<img src={ingredient.image} alt={ingredient.name} />
+			<div className={itemStyles.currency}>
+				<span className='text text_type_digits-default mr-2'>{ingredient.price}</span>
+				<CurrencyIcon />
+			</div>
+			<p className='text text_type_main-default'>{ingredient.name}</p>
+		</div>
+	)
+}
+
+export default BurgerIngredientsItem;
--- src/components/burger-ingredients/BurgerIngredients.jsx
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import ingredientsStyles from './BurgerIngredients.module.css';
-import { Counter, CurrencyIcon, Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import data from '../../utils/data.json';
+import BurgerIngredientsItem from '../burger-ingredients-item/BurgerIngredientsItem';
+
+const makeIngredientsList = (array, type) => {
+	return array.map(item => item.type === type &&
+		<BurgerIngredientsItem key={item._id} ingredient={item} />)
+}
 
 const BurgerIngredients = () => {
-	const [current, setCurrent] = React.useState('rolls');
+	const [current, setCurrent] = React.useState('buns');
 	
 	return (
 		<section className='pt-10'>
 			<h1 className='text text_type_main-large mb-5'>Соберите бургер</h1>
 			<div className={ingredientsStyles.tabs}>
-				<Tab value='rolls' active={current === 'rolls'} onClick={setCurrent}>Булки</Tab>
+				<Tab value='buns' active={current === 'buns'} onClick={setCurrent}>Булки</Tab>
 				<Tab value='sauces' active={current === 'sauces'} onClick={setCurrent}>Соусы</Tab>
-				<Tab value='fillings' active={current === 'fillings'} onClick={setCurrent}>Начинки</Tab>
+				<Tab value='main' active={current === 'main'} onClick={setCurrent}>Начинки</Tab>
 			</div>
-
+			<div className={`${ingredientsStyles.ingredients} custom-scroll`}>
+				<section>
+					<h2 className='text text_type_main-medium' id='bun'>Булки</h2>
+					<div className={ingredientsStyles['ingredients-item']}>
+						{makeIngredientsList(data, 'bun')}
+					</div>
+				</section>
+				<section>
+					<h2 className='text text_type_main-medium' id='sauce'>Соусы</h2>
+					<div className={ingredientsStyles['ingredients-item']}>
+						{makeIngredientsList(data, 'sauce')}
+					</div>
+				</section>
+				<section>
+					<h2 className='text text_type_main-medium' id='main'>Начинки</h2>
+					<div className={ingredientsStyles['ingredients-item']}>
+						{makeIngredientsList(data, 'main')}
+					</div>
+				</section>
+			</div>
 		</section>
 	)
 }
