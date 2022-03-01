f9d34c9d242cb3c189647327fa2caf50fa3d7090
ruslanyar <styxrus@yandex.ru>
2022-03-01 21:38:00 +0300

    Used api instead hard-coded data

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import AppHeader from '../app-header/app-header';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 
-import data from '../../utils/data.json';
+import { api } from '../../utils/constants';
 
 import appStyles from './app.module.css';
 
 const App = () => {
+	const [ingredientsData, setIngredientsData] = useState(null);
 
-	return (
+	useEffect(() => {
+		const getIngredientsData = async () => {
+			try {
+				const res = await fetch(api);
+				const data = await res.json();
+
+				setIngredientsData(data.data);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+
+		getIngredientsData();
+	}, []);
+
+	return ingredientsData && (
 		<>
 			<AppHeader />
 			<main className={appStyles.main}>
-				<BurgerIngredients ingredients={data} />
-				<BurgerConstructor ingredients={data} />
+				<BurgerIngredients ingredients={ingredientsData} />
+				<BurgerConstructor ingredients={ingredientsData} />
 			</main>
 		</>
 	)
