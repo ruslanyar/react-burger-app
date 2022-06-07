5bf958184470e36a4e710536ebd2e24b8e1df97a
ruslanyar <styxrus@yandex.ru>
2022-06-07 17:53:25 +0300

    ref/ingredient-icon: position to props

--- src/components/ingredient-icon/ingredient-icon.jsx
+++ src/components/ingredient-icon/ingredient-icon.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import clsx from 'clsx';
 
 import styles from './ingredient-icon.module.css';
 
-export default function IngredientIcon({ imageUrl, index }) {
+export default function IngredientIcon({
+  imageUrl,
+  index,
+  position = 'absolute',
+}) {
+
+  const ingredientIconStyle = useMemo(() => (
+    position === 'absolute'
+      ? { position, zIndex: `${10 - index}`, left: `${index * 48}px` }
+      : { position }),
+    [index, position]
+  );
+
   return (
-    <div
-      style={{ zIndex: `${10 - index}`, left: `${index * 48}px` }}
-      className={styles.ingredients}
-    >
-      <div className={styles['image-container']}>
-        <div className={styles.border} />
-        <div
-          style={{
-            backgroundImage: `url('${imageUrl}')`,
-          }}
-          className={styles.image}
-        />
-      </div>
-      {index && index === 5 && (
+    <div style={ingredientIconStyle} className={styles['ingredient-icon']}>
+      <div className={styles.border} />
+      <div
+        style={{
+          backgroundImage: `url('${imageUrl}')`,
+        }}
+        className={styles.image}
+      />
+      {index === 5 && (
         <div className={styles.overlay}>
-          <span className={clsx('text', 'text_type_main-small')}>{`+${3}`}</span>
+          <span className={clsx('text', 'text_type_main-small')}>
+            {`+${3}`}
+          </span>
         </div>
       )}
     </div>
