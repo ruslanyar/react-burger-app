af2dadf1d4a2bf5c66eae016e778ccbe1ed93476
ruslanyar <styxrus@yandex.ru>
2022-06-13 23:20:47 +0300

    feat: order-info

--- src/components/order-info/order-info.jsx
+++ src/components/order-info/order-info.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import clsx from 'clsx';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import IngredientIcon from '../ingredient-icon/ingredient-icon';
 
+import { getOrders } from '../../services/selectors';
+import { wsClose, wsConnectionStart } from '../../services/actions';
+
 import styles from './order-info.module.css';
+import { formatOrderNumber } from '../../utils/utils';
+import Loader from '../../ui/loader/Loader';
 
 export default function OrderInfo() {
+  const { id } = useParams();
+  const { orders } = useSelector(getOrders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(wsConnectionStart());
+
+    return () => dispatch(wsClose());
+  }, [dispatch]);
+
+  if (!orders) return <Loader />;
+
+  const order = orders.find((order) => order._id === id);
+  const { name, number, status } = order;
+  const orderNumber = `#${formatOrderNumber(number)}`;
+  const orderStatus = status // TODO
+
   return (
-    <div className="">
-      <span className="">#034533</span>
-      <h2>Black Hole Singularity острый бургер</h2>
-      <span>Выполнен</span>
-      <h3>Состав:</h3>
+    <div className={clsx(styles.container, 'pt-5')}>
+      <p className={clsx('text', 'text_type_digits-default', 'mb-10')}>{orderNumber}</p>
+      <h2 className={clsx('text', 'text_type_main-medium', 'mb-3')}>{name}</h2>
+      <p className={clsx('text', 'text_type_main-default', 'mb-15')}>{orderStatus}</p>
+      <h3 className={clsx('text', 'text_type_main-medium', 'mb-6')}>Состав:</h3>
 
       <ul
         className={clsx(
--- src/services/selectors/wsSelector.js
+++ src/services/selectors/wsSelector.js
@@ -1,1 +1,2 @@
+export const wsSelector = (store) => store.ws;
 export const getOrders = (store) => store.ws.orders;
