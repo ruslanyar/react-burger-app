fd74e8868f5cefb31c34839a8e85cc6345a09811
ruslanyar <styxrus@yandex.ru>
2022-07-03 11:24:01 +0300

    feat: reconfigure store by redux/toolkit

--- src/services/store.js
+++ src/services/store.js
@@ -1,23 +1,16 @@
-import { compose, applyMiddleware, legacy_createStore as createStore } from 'redux';
-import thunk from 'redux-thunk';
-
+import { configureStore } from '@reduxjs/toolkit';
 import { socketMiddleware } from './middleware';
 import { rootReducer } from './reducers';
 
 import { wsAuthUrl, wsUrl } from '../utils/constants';
 import { wsActions, wsAuthActions } from '../utils/ws';
 
-const composeEnhancer =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancer(
-  applyMiddleware(
-    thunk,
-    socketMiddleware(wsUrl, wsActions),
-    socketMiddleware(wsAuthUrl, wsAuthActions, true),
-  )
-);
-
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      socketMiddleware(wsUrl, wsActions),
+      socketMiddleware(wsAuthUrl, wsAuthActions, true)
+    ),
+  devTools: process.env.NODE_ENV !== 'production',
+});
