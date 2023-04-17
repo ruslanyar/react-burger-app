db5d1b8d47aa4a701eec0b0a421bf0bb12478839
ruslanyar <styxrus@yandex.ru>
2023-04-17 18:52:05 +0300

    change HashRouter on BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from './services/store';
 import './index.css';
 import App from './components/app/app';
@@ -10,9 +10,9 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
